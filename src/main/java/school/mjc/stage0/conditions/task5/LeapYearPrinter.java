a65cef519a532640fbbdb77546785998d0198f51
Sergey <spapoyan1987@gmail.com>
2022-12-16 15:49:20 +0400

    commit

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+        if (year == 2000 || year == 2004) {
+            System.out.println("leap");
+        }if (year == 1900) {
+            System.out.println("not leap");
+        }
     }
 }
